b283f3851d36695802606453b7e16a61205a7c9f
Chai <Chai@Chai-PC>
2016-03-25 06:47:53 +0700

    Add Link in About Menu

--- src/chai_4d/mbus/map/screen/HelpDialog.java
+++ src/chai_4d/mbus/map/screen/HelpDialog.java
@@ -1,5 +1,6 @@
 package chai_4d.mbus.map.screen;
 
+import java.awt.Desktop;
 import java.awt.Dimension;
 import java.awt.Frame;
 import java.awt.GridBagConstraints;
@@ -8,6 +9,7 @@ import java.awt.HeadlessException;
 import java.awt.Insets;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.net.URI;
 
 import javax.swing.JButton;
 import javax.swing.JDialog;
@@ -22,6 +24,22 @@ public class HelpDialog extends JDialog implements ActionListener
 {
     private static final long serialVersionUID = 4257633619868006104L;
 
+    private static URI uri;
+
+    private static void open(URI uri)
+    {
+        if (Desktop.isDesktopSupported())
+        {
+            try
+            {
+                Desktop.getDesktop().browse(uri);
+            }
+            catch (Exception e)
+            {
+            }
+        }
+    }
+
     public HelpDialog(Frame owner, String title) throws HeadlessException
     {
         super(owner, title, true);
@@ -30,6 +48,22 @@ public class HelpDialog extends JDialog implements ActionListener
 
     private void initialize()
     {
+        try
+        {
+            uri = new URI(MainFrame.URL);
+        }
+        catch (Exception e)
+        {
+        }
+        class OpenUrlAction implements ActionListener
+        {
+            @Override
+            public void actionPerformed(ActionEvent e)
+            {
+                open(uri);
+            }
+        }
+
         JLabel programName = new JLabel(MainFrame.TITLE_NAME);
         programName.setFont(programName.getFont().deriveFont(18f));
 
@@ -41,53 +75,43 @@ public class HelpDialog extends JDialog implements ActionListener
 
         JPanel content = new JPanel(new GridBagLayout());
         content.setBackground(MapConstants.controlPanel);
-        content.setPreferredSize(new Dimension(340, 200));
-        content.add(programName, new GridBagConstraints(0, 0, 1, 1, 1.0, 0.0, GridBagConstraints.WEST, GridBagConstraints.HORIZONTAL, new Insets(
-            0,
-            5,
-            15,
-            5), 0, 0));
-        content.add(new JLabel("Version : " + MainFrame.VERSION), new GridBagConstraints(
-            0,
-            1,
-            1,
-            1,
-            1.0,
-            0.0,
-            GridBagConstraints.WEST,
-            GridBagConstraints.HORIZONTAL,
-            new Insets(2, 5, 2, 5),
-            0,
-            0));
-        content.add(new JLabel("Author : " + MainFrame.AUTHOR), new GridBagConstraints(
-            0,
-            2,
-            1,
-            1,
-            1.0,
-            0.0,
-            GridBagConstraints.WEST,
-            GridBagConstraints.HORIZONTAL,
-            new Insets(2, 5, 12, 5),
-            0,
-            0));
-        content.add(programDesc, new GridBagConstraints(0, 3, 1, 1, 1.0, 1.0, GridBagConstraints.WEST, GridBagConstraints.BOTH, new Insets(
-            2,
-            5,
-            12,
-            5), 0, 0));
-        content.add(new JLabel("Copyright @ 2008, All rights reserved."), new GridBagConstraints(
-            0,
-            4,
-            1,
-            1,
-            1.0,
-            0.0,
-            GridBagConstraints.WEST,
-            GridBagConstraints.HORIZONTAL,
-            new Insets(2, 5, 2, 5),
-            0,
-            0));
+        content.setPreferredSize(new Dimension(340, 210));
+        content.add(
+            programName,
+            new GridBagConstraints(0, 0, 1, 1, 1.0, 0.0, GridBagConstraints.WEST, GridBagConstraints.HORIZONTAL, new Insets(0, 5, 15, 5), 0, 0));
+        content.add(
+            new JLabel("Version : " + MainFrame.VERSION),
+            new GridBagConstraints(0, 1, 1, 1, 1.0, 0.0, GridBagConstraints.WEST, GridBagConstraints.HORIZONTAL, new Insets(2, 5, 2, 5), 0, 0));
+        content.add(
+            new JLabel("Author : " + MainFrame.AUTHOR),
+            new GridBagConstraints(0, 2, 1, 1, 1.0, 0.0, GridBagConstraints.WEST, GridBagConstraints.HORIZONTAL, new Insets(2, 5, 2, 5), 0, 0));
+
+        JLabel lblURL = new JLabel("URL : ");
+        JButton lnkURL = new JButton();
+        lnkURL.setText("<HTML><FONT color=\"#000099\"><U>" + MainFrame.URL + "</U></FONT></HTML>");
+        lnkURL.setHorizontalAlignment(SwingConstants.LEFT);
+        lnkURL.setBorderPainted(false);
+        lnkURL.setOpaque(false);
+        lnkURL.setBackground(MapConstants.controlPanel);
+        lnkURL.setMargin(new Insets(0, 0, 0, 0));
+        lnkURL.setToolTipText(uri.toString());
+        lnkURL.addActionListener(new OpenUrlAction());
+
+        JPanel linkPanel = new JPanel(new GridBagLayout());
+        linkPanel.setBackground(MapConstants.controlPanel);
+        linkPanel
+            .add(lblURL, new GridBagConstraints(0, 0, 1, 1, 0.0, 0.0, GridBagConstraints.WEST, GridBagConstraints.HORIZONTAL, new Insets(0, 0, 0, 0), 0, 0));
+        linkPanel
+            .add(lnkURL, new GridBagConstraints(1, 0, 1, 1, 1.0, 0.0, GridBagConstraints.WEST, GridBagConstraints.HORIZONTAL, new Insets(0, 0, 0, 0), 0, 0));
+
+        content.add(
+            linkPanel,
+            new GridBagConstraints(0, 3, 1, 1, 1.0, 0.0, GridBagConstraints.WEST, GridBagConstraints.HORIZONTAL, new Insets(2, 5, 12, 5), 0, 0));
+
+        content.add(programDesc, new GridBagConstraints(0, 4, 1, 1, 1.0, 1.0, GridBagConstraints.WEST, GridBagConstraints.BOTH, new Insets(2, 5, 12, 5), 0, 0));
+        content.add(
+            new JLabel("Copyright @ 2008, All rights reserved."),
+            new GridBagConstraints(0, 5, 1, 1, 1.0, 0.0, GridBagConstraints.WEST, GridBagConstraints.HORIZONTAL, new Insets(2, 5, 2, 5), 0, 0));
 
         JButton butOK = new JButton("OK");
         butOK.setPreferredSize(new Dimension(80, 25));
@@ -95,35 +119,12 @@ public class HelpDialog extends JDialog implements ActionListener
 
         JPanel contentPane = new JPanel(new GridBagLayout());
         contentPane.setBackground(MapConstants.controlPanel);
-        contentPane.add(content, new GridBagConstraints(0, 0, 1, 1, 1.0, 1.0, GridBagConstraints.CENTER, GridBagConstraints.BOTH, new Insets(
-            2,
-            2,
-            2,
-            2), 0, 0));
-        contentPane.add(new JSeparator(SwingConstants.HORIZONTAL), new GridBagConstraints(
-            0,
-            1,
-            1,
-            1,
-            1.0,
-            0.0,
-            GridBagConstraints.CENTER,
-            GridBagConstraints.HORIZONTAL,
-            new Insets(2, 2, 2, 2),
-            0,
-            0));
-        contentPane.add(butOK, new GridBagConstraints(
-            0,
-            2,
-            1,
-            1,
-            1.0,
-            0.0,
-            GridBagConstraints.EAST,
-            GridBagConstraints.NONE,
-            new Insets(5, 10, 7, 7),
-            0,
-            0));
+        contentPane
+            .add(content, new GridBagConstraints(0, 0, 1, 1, 1.0, 1.0, GridBagConstraints.CENTER, GridBagConstraints.BOTH, new Insets(2, 2, 2, 2), 0, 0));
+        contentPane.add(
+            new JSeparator(SwingConstants.HORIZONTAL),
+            new GridBagConstraints(0, 1, 1, 1, 1.0, 0.0, GridBagConstraints.CENTER, GridBagConstraints.HORIZONTAL, new Insets(2, 2, 2, 2), 0, 0));
+        contentPane.add(butOK, new GridBagConstraints(0, 2, 1, 1, 1.0, 0.0, GridBagConstraints.EAST, GridBagConstraints.NONE, new Insets(5, 10, 7, 7), 0, 0));
 
         setContentPane(contentPane);
         pack();
--- src/chai_4d/mbus/map/screen/MainFrame.java
+++ src/chai_4d/mbus/map/screen/MainFrame.java
@@ -40,6 +40,7 @@ public class MainFrame extends JFrame
     public static final String TITLE_NAME = "m-Bus Map Screen";
     public static final String VERSION = "1.0";
     public static final String AUTHOR = "Chai_4D";
+    public static final String URL = "http://m-bus.in.th";
 
     private static final String ICON_PATH = "/icons/mbus_icon.png";
 
